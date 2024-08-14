936ea3686b399b17548c579d4b31d77e2d2cdd45
taeyoung kang <kty1467@naver.com>
2024-08-14 20:13:48 +0900

    fix : 이메일 검증 추가

--- src/main/java/com/sparta/springprepare/dto/ManagerDto.java
+++ src/main/java/com/sparta/springprepare/dto/ManagerDto.java
@@ -3,6 +3,7 @@ package com.sparta.springprepare.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -14,5 +15,6 @@ public class ManagerDto {
     private String managerName;
     @Email
     @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$")
     private String email;
 }
